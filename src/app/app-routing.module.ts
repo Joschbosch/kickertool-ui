fdf2014e0d907c35ed080af39410288f31a46b2e
dacl <Daniel.Cleemann@parcit.de>
2024-02-26 13:17:50 +0100

    routing with hashstrategy, versioning

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {path: 'tournamentshow', component: TournamentViewerComponent}
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PanelComponent } from './views/base/panel/panel.component';
@@ -43,7 +42,7 @@ import { TournamentComponent } from './views/tournament/tournament.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [ { provide: LocationStrategy, useClass: HashLocationStrategy } ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
